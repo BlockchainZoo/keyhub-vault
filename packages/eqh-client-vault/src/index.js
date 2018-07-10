be5affa18d101cd0178234be8cb3059ed71f28ff
heri16 <heri16@github.com>
2018-07-11 04:23:57 +0800

    First working version of set.account.property in vault

--- packages/eqh-client-vault/src/index.js
+++ packages/eqh-client-vault/src/index.js
@@ -1,22 +1,44 @@
 const loader = require('./js/nrs.node.bridge')
+// import loader from './js/nrs.cheerio.bridge'
+
+const loadNRS = (config) => {
+  loader.init({
+    url: config.url,
+    secretPhrase: config.secretPhrase,
+    isTestNet: config.isTestNet,
+    adminPassword: config.adminPassword,
+  })
+
+  return new Promise((resolve, reject) => {
+    try {
+      loader.load(resolve)
+    } catch (err) {
+      reject(err)
+    }
+  })
+}
 
 const config = require('./config.json')
 
-loader.init({
-  url: config.url,
-  secretPhrase: config.secretPhrase,
-  isTestNet: config.isTestNet,
-  adminPassword: config.adminPassword,
-})
-
-const ready = new Promise((resolve, reject) => {
-  try {
-    loader.load(resolve)
-  } catch (err) {
-    reject(err)
-  }
-})
-
-ready.then(() => {
+loadNRS(config).then((NRS) => {
   console.log('module configured and ready') // eslint-disable-line no-console
+
+  const property = '$$RootAdmin'
+
+  const recipient = NRS.getAccountIdFromPublicKey(config.recipientPublicKey)
+  const recipientRS = NRS.convertNumericToRSAccountFormat(recipient)
+  console.log(recipient)
+  console.log(recipientRS)
+
+  const data = {
+    recipient,
+    property,
+    value: '1',
+    secretPhrase: config.secretPhrase,
+    ...NRS.getMandatoryParams(),
+  }
+
+  NRS.sendRequest('setAccountProperty', data, (response) => {
+    NRS.logConsole(JSON.stringify(response))
+  })
 })
