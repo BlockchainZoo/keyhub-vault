5e6de3d476d65f1cd390cedc75758bb3fc9c06b2
heri16 <heri16@github.com>
2018-07-11 04:23:57 +0800

    First working version of set.account.property in vault

---
+++ packages/eqh-client-vault/src/sample/set.account.property.js
@@ -0,0 +1,19 @@
+const loader = require('./loader')
+
+const { config } = loader
+
+loader.load((NRS) => {
+  const property = '$$RootAdmin'
+
+  const data = {
+    recipient: NRS.getAccountIdFromPublicKey(config.recipientPublicKey),
+    property,
+    value: '1',
+    secretPhrase: config.secretPhrase,
+    ...NRS.getMandatoryParams(),
+  }
+
+  NRS.sendRequest('setAccountProperty', data, (response) => {
+    NRS.logConsole(JSON.stringify(response))
+  })
+})
