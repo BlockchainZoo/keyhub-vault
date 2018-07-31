94b9a71aa3d0c50f94fbafd0db73494302838c60
heri16 <heri16@github.com>
2018-07-31 11:43:35 +0800

    Add error handling to vault

--- packages/eqh-client-vault/src/screen/welcome.js
+++ packages/eqh-client-vault/src/screen/welcome.js
@@ -5,10 +5,11 @@ export default function createElement(document, hasAccounts) {
   div.innerHTML = (safeHtml`
   <h2 class="page-title">Welcome</h2>
   <div class="entry-content">
-    <p>Welcome to KeyHub!</p>
+    <p>Welcome to KeyHub, your home for secure key storage!</p>
     <p>Keyhub is "Paypal for Blockchain".</p>
     <p>Using Paypal ensures that merchant websites you visit do not get permanent access to your sensitive credit-card data.</p>
-    <p>Using Keyhub ensures that merchant websites you visit do not get permanent access to your private key data.</p>
+    <p>Using Keyhub ensures that merchant websites you visit do not get permanent access to your private keys.</p>
+    <p>We at Keyhub take security seriously. Private Key material are generated locally inside your browser. Keys never leave your browser. Keys never reach the web.</p>
     <p></p>
     <p>${hasAccounts ? 'Please select one of your accounts on the left sidebar to continue.' : 'Click "Add Key" on the left sidebar to begin.'}</p>
   </div>
