18c626f069bc83d688c62f3c2dafd1d607440b42
Hardi Suputra <cokadie@gmail.com>
2018-08-10 11:52:45 +0800

    FE Vault: Bulk Update and Add new font (#125)
    * FE Vault: Bulk Update and Add new font
    
    * Vault UI: Add account detail interface

--- packages/eqh-client-vault/src/screen/tx-detail.js
+++ packages/eqh-client-vault/src/screen/tx-detail.js
@@ -16,31 +16,70 @@ const validatePin = (pinInput, pinAlert) => {
   return false
 }
 
+const splitNumber = (accountNo) => {
+  const str = accountNo.toString()
+  const result = [str[0]]
+
+  for (let x = 1; x < str.length; x += 1) {
+    if ((x % 5 === 0)) {
+      result.push('.', str[x])
+    } else {
+      result.push(str[x])
+    }
+  }
+  return result.join('')
+}
+
 const displayTransaction = (document, detailDiv, { type, data }) => {
   const domFragment = document.createDocumentFragment()
+  let txData = ''
+
+  const divTxTypeRow = document.createElement('div')
+  divTxTypeRow.classList.add('row', 'my-2', 'border-bottom', 'py-3')
+
+  const divTxTypeLabel = document.createElement('div')
+  divTxTypeLabel.classList.add('col-sm-4', 'text-capitalize', 'text-strong')
+  divTxTypeLabel.appendChild(document.createTextNode('Type'))
+
+  // const divTxIcon = document.createElement('div')
+  // divTxIcon.classList.add('bullet-icon')
+
+  const divTxIconFa = document.createElement('i')
+  divTxIconFa.classList.add('fas', 'fa-money-bill-alt', 'text-secondary', 'mr-2')
+  // divTxIcon.appendChild(divTxIconFa)
 
   const divTxType = document.createElement('div')
+  divTxType.classList.add('col-sm-8', 'text-grey')
+
+  divTxType.appendChild(divTxIconFa)
   divTxType.appendChild(document.createTextNode(type))
-  domFragment.appendChild(divTxType)
+
+  divTxTypeRow.appendChild(divTxTypeLabel)
+  divTxTypeRow.appendChild(divTxType)
+  domFragment.appendChild(divTxTypeRow)
 
   Object.keys(data).forEach((key) => {
     const divRow = document.createElement('div')
-    divRow.classList.add('row', 'flex-nowrap', 'my-2')
+    divRow.classList.add('row', 'my-2', 'border-bottom', 'py-3')
 
     const divKey = document.createElement('div')
-    divKey.classList.add('col-5')
+    divKey.classList.add('col-sm-4', 'text-capitalize', 'text-strong')
+
     divKey.appendChild(document.createTextNode(key))
 
-    const divColon = document.createElement('div')
-    divColon.classList.add('col-1', 'd-none', 'd-md-flex')
-    divColon.innerHTML = ':'
+    // const divColon = document.createElement('div')
+    // divColon.classList.add('col-sm-1', 'd-none', 'd-sm-flex')
+    // divColon.innerHTML = ':'
 
     const divVal = document.createElement('div')
-    divVal.classList.add('col-6')
-    divVal.appendChild(document.createTextNode(data[key]))
+    divVal.classList.add('col-sm-8', 'text-grey')
+
+    txData = (key === 'recipient') ? splitNumber(data[key]) : data[key]
+
+    divVal.appendChild(document.createTextNode(txData))
 
     divRow.appendChild(divKey)
-    divRow.appendChild(divColon)
+    // divRow.appendChild(divColon)
     divRow.appendChild(divVal)
 
     domFragment.appendChild(divRow)
@@ -49,32 +88,43 @@ const displayTransaction = (document, detailDiv, { type, data }) => {
   detailDiv.appendChild(domFragment)
 }
 
-export default function createElement(document, platform, accountNo, tx, callback) {
+export default function createElement(document, platform, accountNo, address, tx, callback) {
   const div = document.createElement('div')
 
   const pinForm = (safeHtml`
-    <div class="form-group">
-      <label for="text-pin">Security Pin: </label><br>
-      <input type="password" class="form-control" id="pin-input" />
+    <div class="col">
+      <div class="form-group">
+        <label for="text-pin"><b>Security Pin</b></label></br>
+        <input type="password" class="form-control" id="pin-input" />
+      </div>
+      <div id="pin-alert" class="d-hide"></div>
     </div>
-    <div id="pin-alert" class="d-hide form-group"></div>
   `)
 
   div.innerHTML = (html`
   <div class="card-header text-center bg-secondary text-white">
-    <h4><i class="fas fa-lock"></i> ${platform} Transaction Detail</h4>
+    <h4 class="mb-2"><i class="fas fa-lock"></i> ${platform} Transaction Detail</h4>
+    <p class="mb-0">Your Account No. ${splitNumber(accountNo)}</p>
+    <small>Account address: ${address}</small>
   </div>
   <div class="card-body">
-    <div>Your Account No.: ${accountNo}</div>
-    <div id="transaction-detail"></div>
-
-    ${pinForm}
-
-    <div class="row justify-content-center col mt-5 mb-2">
-      <button class="btn btn-primary px-4" id="sign-tx" data-choice="ok"><i class="fas fa-sign-in-alt"></i> SIGN THIS</button>
+    <div class="row">
+      <div class="col">
+        <div id="transaction-detail"></div>
+      </div>
     </div>
-    <div class="row justify-content-center col mt-5 mb-2">
-      <button class="btn btn-danger px-4" id="cancel-sign-tx" data-choice="cancel">CANCEL</button>
+
+    <div class="row justify-content-center mt-5">
+      ${pinForm}
+    </div>
+    <div class="row justify-content-center">
+      <div class="col col-md-5 mb-3">
+        <button class="btn btn-primary px-4 btn-block" id="sign-tx" data-choice="ok"><i class="fas fa-sign-in-alt"></i> SIGN THIS</button>
+      </div>
+      <div class="col col-md-5">
+        <button class="btn btn-danger px-4 btn-block" id="cancel-sign-tx" data-choice="cancel">CANCEL</button>
+      </div>
+    </div>
     </div>
   </div>`)
 
