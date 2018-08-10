d47d7846e1f1f46949e7d7df1d9ed5945a44b893
heri16 <heri16@github.com>
2018-08-10 19:41:50 +0800

    Merge KYC and PrivateKey into AccountSetup

--- packages/eqh-client-vault/src/screen/welcome.js
+++ packages/eqh-client-vault/src/screen/welcome.js
@@ -5,11 +5,16 @@ export default function createElement(document, hasAccounts) {
   div.innerHTML = safeHtml`
   <h2 class="page-title">Welcome</h2>
   <div class="entry-content">
-    <p>Welcome to KeyHub, your home for secure key storage!</p>
-    <p>Keyhub is "Paypal for Blockchain".</p>
+    <p>Welcome to KeyHub, an open-source app for secure key storage!</p>
+    <p>Keyhub is "Paypal for Blockchain"</p>
     <p>Using Paypal ensures that merchant websites you visit do not get permanent access to your sensitive credit-card data.</p>
     <p>Using Keyhub ensures that merchant websites you visit do not get permanent access to your private keys.</p>
+    <p></p>
     <p>We at Keyhub take security seriously. Private Key material are generated locally inside your browser. Keys never leave your browser. Keys never reach the web.</p>
+    <p>
+      KeyHub has been audited by the
+      <a href="https://observatory.mozilla.org/analyze/vault.keyhub.app" target="_blank"> Mozilla Foundation here.</a>
+    </p>
     <p></p>
     <p>${
       hasAccounts
