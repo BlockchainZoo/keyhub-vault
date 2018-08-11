60ac1b5914290c450d15acf24d76c189a172b7f9
heri16 <heri16@github.com>
2018-08-12 02:45:35 +0800

    Update wording used in eqh-vault

--- packages/eqh-client-vault/src/screen/welcome.js
+++ packages/eqh-client-vault/src/screen/welcome.js
@@ -6,11 +6,11 @@ export default function createElement(document, hasAccounts) {
   <h2 class="page-title">Welcome</h2>
   <div class="entry-content">
     <p>Welcome to KeyHub, an open-source app for secure key storage!</p>
-    <p>Keyhub is "Paypal for Blockchain"</p>
+    <p>KeyHub is "Paypal for Blockchain"</p>
     <p>Using Paypal ensures that merchant websites you visit do not get permanent access to your sensitive credit-card data.</p>
-    <p>Using Keyhub ensures that merchant websites you visit do not get permanent access to your private keys.</p>
+    <p>Using KeyHub ensures that merchant websites you visit do not get permanent access to your private keys.</p>
     <p></p>
-    <p>We at Keyhub take security seriously. Private Key material are generated locally inside your browser. Keys never leave your browser. Keys never reach the web.</p>
+    <p>We at KeyHub take security seriously. Private Key material are generated locally inside your browser. Keys never leave your browser. Keys never reach the web.</p>
     <p>
       KeyHub has been audited by the
       <a href="https://observatory.mozilla.org/analyze/vault.keyhub.app" target="_blank"> Mozilla Foundation here.</a>
