7d3d8dba657b99b40c23a065af9924ce35a3a71b
Heri Sim <527101+heri16@users.noreply.github.com>
2018-10-19 09:27:49 +0800

    EQH-176: Add "Restore Missing Key" screen to KeyHub Vault (#348)
    * Refactor to cleanup webworker API
    
    * Add key restore feature to vault

--- packages/eqh-client-vault/src/screen/error.js
+++ packages/eqh-client-vault/src/screen/error.js
@@ -1,29 +1,30 @@
 import { safeHtml } from 'common-tags'
 
-export default function createElement(document, title, message, callback) {
+export default function createElement(document, title, message) {
   const div = document.createElement('div')
-  div.innerHTML = safeHtml`<div class="card card-error-message">
-  <div class="card-header text-center bg-danger text-white">
-      <h4><i class="fas fa-exclamation-triangle"></i> ${title}</h4>
-  </div>
-  <div class="card-body">
-      <p>${message}</p>
-      <div class="text-center">
-        <button class="btn btn-primary" id="back-to-app" data-choice="ok"><i class="fas fa-mobile-alt"></i> Back to App</button>
-      </div>
-    </div>
-  </div>`
 
-  if (callback) {
+  div.innerHTML = safeHtml`<div class="card card-error-message">
+    <div class="card-header text-center bg-danger text-white">
+        <h4><i class="fas fa-exclamation-triangle"></i> ${title}</h4>
+    </div>
+    <div class="card-body">
+        <p>${message}</p>
+        <div class="text-center">
+          <button class="btn btn-primary" id="back-to-app" data-choice="ok"><i class="fas fa-mobile-alt"></i> Back to App</button>
+        </div>
+      </div>
+    </div>`
+
+  const promise = new Promise(resolve => {
     div.querySelectorAll('button').forEach(b =>
       b.addEventListener('click', ev => {
         const {
           dataset: { choice },
         } = ev.currentTarget
-        callback(null, choice)
+        resolve(choice)
       })
     )
-  }
+  })
 
-  return div
+  return [div, promise]
 }
