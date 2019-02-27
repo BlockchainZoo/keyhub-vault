639bf2215b4b7b2783016913e92bc543e19363e7
heri16 <527101+heri16@users.noreply.github.com>
2019-02-27 20:50:06 +0800

    Update network config of equinehub-dev

--- packages/keyhub-vault-web/src/conf/equinehub-dev/nxt.js
+++ packages/keyhub-vault-web/src/conf/equinehub-dev/nxt.js
@@ -1,11 +1,11 @@
 'use strict'
 
 module.exports = {
-  url: 'https://nxt1.vault.keyhub.app',
+  url: 'https://nxt1.vault.sandbox.keyhub.app',
   isTestNet: false,
   adminPassword: '',
   lastKnownBlock: {
-    // See: https://nxt1.vault.keyhub.app/test?requestType=getECBlock
+    // See: https://nxt1.vault.sandbox.keyhub.app/test?requestType=getECBlock
     id: '3493682855030002679',
     height: '515',
   },
